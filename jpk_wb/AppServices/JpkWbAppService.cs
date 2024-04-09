f977a57b6ef35b1a1c7efe14cf44edd5b64eb302
Krzysztof <krzysztof.andrzej.jurkowski@gmail.com>
2024-04-09 17:19:01 +0200

    init

---
+++ jpk_wb/AppServices/JpkWbAppService.cs
@@ -0,0 +1,14 @@
+﻿namespace jpk_wb.AppServices;
+
+public interface IJpkWbAppService
+{
+    Task Run(string file, string output);
+}
+
+public class JpkWbAppService : IJpkWbAppService
+{
+    public async Task Run(string file, string output)
+    {
+        Console.WriteLine("lmao");
+    }
+}
---
+++ jpk_wb/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.CommandLine;
+using jpk_wb.AppServices;
+using Microsoft.Extensions.DependencyInjection;
+
+ServiceCollection services = new ServiceCollection();
+
+services.AddTransient<IJpkWbAppService, JpkWbAppService>();
+
+var serviceProvider = services.BuildServiceProvider();
+
+var rootCommand = new RootCommand("JPK_WB parser");
+
+var fileOption = new Option<string>("--file",
+                                     description: "File to parse");
+ fileOption.AddAlias("-f");
+ fileOption.IsRequired = true;
+ rootCommand.AddOption(fileOption);
+ 
+var outputOption = new Option<string>("--output",
+                                      description: "Output file",
+                                      getDefaultValue: () => "jpk_wb.xml");
+outputOption.AddAlias("-o");
+rootCommand.AddOption(outputOption);
+
+
+rootCommand.SetHandler(
+    async (fileOptionValue, outputOptionValue) =>
+    {
+        await serviceProvider.GetService<IJpkWbAppService>()!.Run(fileOptionValue, outputOptionValue);
+    },
+    fileOption, outputOption);
+    
+rootCommand.InvokeAsync(args).Wait();
