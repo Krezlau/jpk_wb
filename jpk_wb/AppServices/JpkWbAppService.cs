70cb5768d6334c879503130a96d9de7c04f55549
Krzysztof <krzysztof.andrzej.jurkowski@gmail.com>
2024-04-09 17:35:59 +0200

    Add additional commands

--- jpk_wb/AppServices/JpkWbAppService.cs
+++ jpk_wb/AppServices/JpkWbAppService.cs
@@ -3,12 +3,31 @@
 public interface IJpkWbAppService
 {
     Task Run(string file, string output);
+    Task AddData(string file);
+    Task DeleteData();
+    Task CreateXml(string output);
 }
 
 public class JpkWbAppService : IJpkWbAppService
 {
     public async Task Run(string file, string output)
     {
-        Console.WriteLine("lmao");
+        await AddData(file);
+        await CreateXml(output);
+    }
+
+    public async Task AddData(string file)
+    {
+        Console.WriteLine("AddData");
+    }
+
+    public async Task DeleteData()
+    {
+        Console.WriteLine("DeleteData");
+    }
+
+    public async Task CreateXml(string output)
+    {
+        Console.WriteLine("CreateXml");
     }
 }
--- jpk_wb/Program.cs
+++ jpk_wb/Program.cs
@@ -23,6 +23,39 @@ var outputOption = new Option<string>("--output",
 outputOption.AddAlias("-o");
 rootCommand.AddOption(outputOption);
 
+var addCommand = new Command("add", "Add data");
+rootCommand.AddCommand(addCommand);
+addCommand.AddAlias("a");
+addCommand.AddOption(fileOption);
+
+var deleteCommand = new Command("delete", "Delete data");
+rootCommand.AddCommand(deleteCommand);
+deleteCommand.AddAlias("d");
+
+var createXmlCommand = new Command("create-xml", "Create XML file");
+rootCommand.AddCommand(createXmlCommand);
+createXmlCommand.AddAlias("c");
+createXmlCommand.AddOption(outputOption);
+
+addCommand.SetHandler(
+    async (fileOptionValue) =>
+    {
+        await serviceProvider.GetService<IJpkWbAppService>()!.AddData(fileOptionValue);
+    },
+    fileOption);
+
+deleteCommand.SetHandler(
+    async () =>
+    {
+        await serviceProvider.GetService<IJpkWbAppService>()!.DeleteData();
+    });
+
+createXmlCommand.SetHandler(
+    async (outputOptionValue) =>
+    {
+        await serviceProvider.GetService<IJpkWbAppService>()!.CreateXml(outputOptionValue);
+    },
+    outputOption);
 
 rootCommand.SetHandler(
     async (fileOptionValue, outputOptionValue) =>
